475dabcf92b90abf7dad3106717144058f867d47
Doyun Jang <ehdbsdl0304@gmail.com>
2023-11-12 03:33:36 +0900

    테스트

--- IQ Test/src/CalcExpr.java
+++ IQ Test/src/CalcExpr.java
@@ -1,14 +1,17 @@
 
+//드디어 해결??
+//제발...
 import java.io.Serializable;
 
+
 public class CalcExpr implements Serializable {
-    double operand1, operand2;
-    char operator;
+	double operand1, operand2;
+	char operator;
 
-    public CalcExpr(double operand1, char operator, double operand2) {
-        this.operand1 = operand1;
-        this.operator = operator;
-        this.operand2 = operand2;       
-    }
+
+	public CalcExpr(double operand1, char operator, double operand2) {
+		this.operand1 = operand1;
+		this.operator = operator;
+		this.operand2 = operand2;
+	}
 }
-
