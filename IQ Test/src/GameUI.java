caf11900c372244051711d3635a8d72162716628
Doyun Jang <ehdbsdl0304@gmail.com>
2023-11-15 21:58:59 +0900

    1. GameUI 수정 -도윤
    2. 산술 문제 출력란 크기 위치 수정
    
    3. Progress Bar 추가 완료

--- IQ Test/src/GameUI.java
+++ IQ Test/src/GameUI.java
@@ -2,7 +2,7 @@
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,16 +12,23 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 public class GameUI extends JFrame {
 
 	private JTextArea t_display;
 	private JTextField t_input;
-	private JTextField t_operand1, t_operator, t_operand2, t_result;
+	private JTextField t_operand1, t_operator1, t_operand2, t_operator2, t_result; // 산술 문제 출제란
+	private JProgressBar progressBar;
+	private JButton levelCompleteButton;
+
+	private int currentLevel = 0;
+	private final int totalLevels = 10;
 
 
 	public GameUI() {
@@ -39,8 +46,10 @@ public class GameUI extends JFrame {
 		add(createDisplayPanel());
 		add(createInputPanel(), BorderLayout.SOUTH);
 		add(createQuestionPanel(), BorderLayout.CENTER);
+		add(addProgressBar(), BorderLayout.NORTH);
 
 	}
+
 
 	private JPanel createDisplayPanel() {
 		JPanel p = new JPanel(new BorderLayout());
@@ -50,15 +59,38 @@ public class GameUI extends JFrame {
 
 		p.add(new JScrollPane(t_display), BorderLayout.CENTER);
 
+		// JLabel levelLabel = new JLabel("Level 1");
+		// levelLabel.setFont(new Font("굴림", Font.PLAIN, 25));
+		// levelLabel.setBounds(26, 10, 130, 84);
+		// add(levelLabel);
 		return p;
 	}
+
+
+	private JPanel addProgressBar() {
+
+		JPanel topPanel = new JPanel(new BorderLayout());
+		JLabel levelLabel = new JLabel("Level 1");
+		levelLabel.setFont(new Font("굴림", Font.PLAIN, 25));
+
+		progressBar = new JProgressBar(0, totalLevels);
+		progressBar.setStringPainted(true);
+		progressBar.setValue(currentLevel);
+
+		topPanel.add(levelLabel, BorderLayout.WEST);
+		topPanel.add(progressBar, BorderLayout.CENTER);
+		add(topPanel, BorderLayout.NORTH);
+
+		return topPanel;
+	}
+
 
 	private JPanel createInputPanel() {
 		JPanel p = new JPanel(new BorderLayout());
 
 		t_input = new JTextField(30);
 		JButton b_send = new JButton("보내기");
-		
+
 		b_send.setPreferredSize(new Dimension(b_send.getPreferredSize().width, 40));
 
 		p.add(t_input, BorderLayout.CENTER);
@@ -73,28 +105,45 @@ public class GameUI extends JFrame {
 
 	private JPanel createQuestionPanel() {
 		// 네프 계산 프로그램 참고
-		JPanel p = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JPanel p = new JPanel(null);
 
-		t_operand1 = new JTextField(5);
-		t_operator = new JTextField(3);
-		t_operand2 = new JTextField(5);
-		t_result = new JTextField(5);
+		t_operand1 = new JTextField(30);
+		t_operator1 = new JTextField(30);
+		t_operand2 = new JTextField(30);
+		t_operator2 = new JTextField(30);
+		t_result = new JTextField(30);
 
-		t_operand1.setHorizontalAlignment(JTextField.RIGHT);
-		t_operator.setHorizontalAlignment(JTextField.CENTER);
-		t_operand2.setHorizontalAlignment(JTextField.RIGHT);
-		t_result.setHorizontalAlignment(JTextField.RIGHT);
-		t_result.setEnabled(false);
+		t_operand1.setHorizontalAlignment(SwingConstants.CENTER);
+		t_operator1.setHorizontalAlignment(SwingConstants.CENTER);
+		t_operand2.setHorizontalAlignment(SwingConstants.CENTER);
+		t_operator2.setHorizontalAlignment(SwingConstants.CENTER);
+		t_result.setHorizontalAlignment(SwingConstants.CENTER);
+
+		t_operand1.setBounds(85, 244, 90, 50);
+		t_operator1.setBounds(225, 244, 60, 50);
+		t_operand2.setBounds(340, 244, 90, 50);
+		t_operator2.setBounds(485, 244, 60, 50);
+		t_result.setBounds(600, 244, 90, 50);
 
 		p.add(t_operand1);
-		p.add(t_operator);
+		p.add(t_operator1);
 		p.add(t_operand2);
-		p.add(new JLabel("="));
+		p.add(t_operator2);
 		p.add(t_result);
 
-		t_operand1.setEnabled(false);
-		t_operator.setEnabled(false);
-		t_operand2.setEnabled(false);
+		t_operator2.setText("=");
+
+		t_operand1.setFont(new Font("굴림", Font.PLAIN, 30));
+		t_operator1.setFont(new Font("굴림", Font.PLAIN, 30));
+		t_operand2.setFont(new Font("굴림", Font.PLAIN, 30));
+		t_operator2.setFont(new Font("굴림", Font.PLAIN, 30));
+		t_result.setFont(new Font("굴림", Font.PLAIN, 30));
+
+		t_operand1.setEditable(false);
+		t_operator1.setEditable(false);
+		t_operand2.setEditable(false);
+		t_operator2.setEditable(false);
+		t_result.setEditable(false);
 
 		return p;
 
