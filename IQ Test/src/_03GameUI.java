9767943fc93a16ca41dc6679c0648c3d4f057d3b
Doyun Jang <ehdbsdl0304@gmail.com>
2023-11-18 23:01:51 +0900

    1. xptmxm

--- IQ Test/src/_03GameUI.java
+++ IQ Test/src/_03GameUI.java
@@ -259,13 +259,13 @@ public class _03GameUI extends JFrame {
 		int num1 = rand.nextInt(35) + 4;
 		int num2 = rand.nextInt(35) + 4;
 
-		String[] operators = { "*", "/" };
+		String[] operators = { "/" };
 		String operator = operators[rand.nextInt(operators.length)];
 
 		int result = 0;
 		if (operator.equals("/")) {
 			List<Integer> excludedNumbers = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 5, 7, 10));
-			while (num1 <= num2 || excludedNumbers.contains(num1) || num1 == num2) {
+			while (num1 <= num2 || excludedNumbers.contains(num1) || num1 != num2) {
 				num1 = rand.nextInt(150) + 1;
 				num2 = rand.nextInt(15) + 2;
 			}
